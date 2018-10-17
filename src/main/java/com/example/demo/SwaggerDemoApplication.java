38e6576a12d184f1193fca225a730201174a798c
lou jian wen <tom.grade1986@163.com>
2018-10-17 18:09:00 +0800

    配置swagger ui
    Signed-off-by: lou jian wen <tom.grade1986@163.com>

--- src/main/java/com/example/demo/SwaggerDemoApplication.java
+++ src/main/java/com/example/demo/SwaggerDemoApplication.java
@@ -2,11 +2,34 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SwaggerDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SwaggerDemoApplication.class, args);
 	}
+
+	@Bean
+	public Docket createRestApi() {
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
+				.apis(RequestHandlerSelectors.basePackage("com.example.demo.act")).paths(PathSelectors.any()).build();
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("Spring Boot中使用Swagger2构建RESTful APIs")
+				.description("更多Spring Boot相关文章请关注：http://blog.didispace.com/")
+				.termsOfServiceUrl("http://blog.didispace.com/").contact("程序猿DD").version("1.0").build();
+	}
+
 }
